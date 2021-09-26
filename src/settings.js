9866de6f53e9608c06388ea2df170189ff8647da
pzh <769875137@qq.com>
2021-09-26 10:25:31 +0800

    no message

--- src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Update.fire",orientation:"portrait",jsList:["assets/script/plugin/pb/library/protobuf-library.min.js"]};
